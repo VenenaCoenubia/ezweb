5f9d8cfdd4c6e506392ca6423af876794c3afa16
chinnawatuth <champ3599@gmai.com>
2022-06-07 22:42:20 +0700

    เพิ่มfor

--- ezweb.js
+++ ezweb.js
@@ -5,37 +5,45 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-let ex1 = []
+let ex = []
 
 //get
 app.get('/getData', (request, response) => {
-    response.send(ex1);
+    response.send(ex);
 })
 
 //post
 app.post('/createData', (request, response) => {
-
-    const new_ex1_data = {
+    const new_ex_data = {
         name_first: request.body.name_first,
-        name_last: request.body.name_last,
+        name_last: request.body.name_last
     }
-
     if (
-        !new_ex1_data.name_first ||
-        !new_ex1_data.name_last
+        !new_ex_data.name_first ||
+        !new_ex_data.name_last
     ) {
         return response.status(400).send("error data not enough");
     }
-    ex1.push(new_ex1_data)
+    for (let i = 0; i < ex.length; i++) {
+        if (
+            ex[i].name_first == new_ex_data.name_first ||
+            ex[i].name_last == new_ex_data.name_last
+        ) {
+            return response.status(400).send("error data duplicate");
+        }
+    }
+
+    ex.push(new_ex_data)
+
     response.send("ok");
 })
 app.delete('/removeData', (request, response) => {
     if (!request.body.name_first) {
         return response.status(400).send("error data not enough");
     }
-    for (let i = 0; i < ex1.length; i++) {
-        if (ex1[i].id == request.body.name_first) {
-            ex1.splice(i, 1);
+    for (let i = 0; i < ex.length; i++) {
+        if (ex[i].id == request.body.name_first) {
+            ex.splice(i, 1);
             return response.send("ok");
         }
     }
@@ -44,4 +52,5 @@ app.delete('/removeData', (request, response) => {
 //listen
 app.listen(3000, () => {
     console.log('server started!');
+    console.log
 });
